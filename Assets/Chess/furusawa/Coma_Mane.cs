95bf1e570cc057e3e8eb0b2b40e333f61bfba876
FurusawaRyutarou <ryuu602104@yahoo.co.jp>
2017-03-06 12:30:35 +0900

    no message

---
+++ Assets/Chess/furusawa/Coma_Mane.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coma_Mane : MonoBehaviour
+{
+    bool komasentaku = false;
+    public GameObject clickMAP;
+    public GameObject clickCUBE;
+    ChessMap chessmap;
+	// Use this for initialization
+	void Start () {
+        chessmap = GetComponent<ChessMap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //メインカメラ上のマウスカーソルのある位置からRayを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (komasentaku == false)
+            {
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.gameObject.tag == ("Coma"))
+                    {
+                        clickCUBE = hit.collider.gameObject;
+                        komasentaku = true;
+                    }
+                }
+            }
+
+            if (komasentaku == true)
+            {
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.gameObject.tag == ("Maptile"))
+                    {
+                        clickMAP = hit.collider.gameObject;
+                        komasentaku = false;
+                    }
+                }
+            }
+            Debug.Log(clickCUBE);
+            Debug.Log(clickMAP);
+        }
+    }
+}
