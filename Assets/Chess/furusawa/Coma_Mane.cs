422796a5df763c346b45fbef38b00abeacda4ca9
FurusawaRyutarou <ryuu602104@yahoo.co.jp>
2017-03-06 15:08:09 +0900

    no message

--- Assets/Chess/furusawa/Coma_Mane.cs
+++ Assets/Chess/furusawa/Coma_Mane.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Coma_Mane : MonoBehaviour
 {
+
+    public GameObject MapTile;
+    public GameObject MapTile2;
+    public GameObject MapTile3;
     bool komasentaku = false;
     public GameObject clickMAP;
     public GameObject clickCUBE;
     ChessMap chessmap;
+    Vector2 comapos;
+    Coma coma;
+    float interval;
 	// Use this for initialization
 	void Start () {
         chessmap = GetComponent<ChessMap>();
+        interval=0.7f;
     }
 
     // Update is called once per frame
@@ -20,17 +28,32 @@ public class Coma_Mane : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Input.GetMouseButtonDown(0))
-        {
-            if (komasentaku == false)
-            {
+        //{
+        //    if (komasentaku == false)
+        //    {
                 if (Physics.Raycast(ray, out hit, Mathf.Infinity))
                 {
-                    if (hit.collider.gameObject.tag == ("Coma"))
-                    {
-                        clickCUBE = hit.collider.gameObject;
-                        komasentaku = true;
-                    }
+                if (hit.collider.gameObject.tag == ("Coma"))
+                {
+                    clickCUBE = hit.collider.gameObject;
+                    coma = clickCUBE.GetComponent<Coma>();
+                    comapos = coma.position;
+                    //周りのコマチェック
+                    if (chessmap.Map_array[(int)comapos.y, (int)comapos.x + 1] == 0 && comapos.x+1 < 6)//→
+                        Instantiate(MapTile, new Vector3(((int)comapos.x + 2 )* interval, 1, (int)comapos.y * interval), Quaternion.Euler(90.0f, 0.0f, 0.0f));
+
+                    if (chessmap.Map_array[(int)comapos.y, (int)comapos.x - 1] == 0 && comapos.x-1 > 0)//左
+                       Instantiate(MapTile, new Vector3(((int)comapos.x - 2) * interval, 1, (int)comapos.y * interval), Quaternion.Euler(90.0f, 0.0f, 0.0f));
+
+                    if (chessmap.Map_array[(int)comapos.y + 1, (int)comapos.x] == 0 && comapos.y+1 < 16)//上
+                        Instantiate(MapTile, new Vector3((int)comapos.x * interval, 1, ((int)comapos.y + 2) * interval), Quaternion.Euler(90.0f, 0.0f, 0.0f));
+
+                    if (chessmap.Map_array[(int)comapos.y - 1, (int)comapos.x] == 0 && comapos.y-1 > 0)//下
+                        Instantiate(MapTile, new Vector3((int)comapos.x * interval, 1, ((int)comapos.y - 2) * interval), Quaternion.Euler(90.0f, 0.0f, 0.0f));
+
+                    komasentaku = true;
                 }
+                //}
             }
 
             if (komasentaku == true)
@@ -48,4 +71,3 @@ public class Coma_Mane : MonoBehaviour
             Debug.Log(clickMAP);
         }
     }
-}
