765867ce87e01524730623e13eb1a45ae72bf1fd
kamosica <nkc.kenta.oishi@gmail.com>
2017-03-07 09:36:30 +0900

    0307朝

--- Assets/Chess/oishi/Coma.cs
+++ Assets/Chess/oishi/Coma.cs
@@ -9,13 +9,31 @@ public class Coma : MonoBehaviour {
     public Vector2 position;
     public bool isCrush; //つぶれているかどうか
 
+    Color coma_color;
+
+    float alfa = 1.0f;
+    bool alfaflg = true;
+
     // Use this for initialization
     void Start () {
-	
-	}
+
+        coma_color = this.GetComponent<Renderer>().material.color;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        this.transform.localScale 
+
+        //if (alfa == 0.0f) alfaflg = false;
+        //else if (alfa == 1.0f) alfaflg = true;
+
+        //if (alfaflg == false) alfa += 0.05f;
+        //else if (alfaflg == true) alfa -= 0.05f;
+
+        //coma_color = new Color(coma_color.r, coma_color.g, coma_color.b, alfa);
+
+        //this.GetComponent<Renderer>().material.color = coma_color;
+
+    }
 }
--- Assets/Chess/oishi/TurnManager.cs
+++ Assets/Chess/oishi/TurnManager.cs
@@ -1,15 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TurnManager: MonoBehaviour {
 
+    public GameObject Turn_Text;
+    public GameObject Turn2_Text;
+
+    Text turn_t;
+    Text turn2_t;
+
+    bool isfirst = true;   //true 先行　false 後攻
+    int turn_count = 0;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        turn_t = Turn_Text.GetComponent<Text>();
+        turn2_t = Turn2_Text.GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    //テキストの変更
+    public void Change_Text()
+    {
+        if (turn_count%2 == 0)
+        {
+            turn_count++;
+            turn_t.text = "後攻";
+
+            turn2_t.text = turn_count / 2 + 1 + "ターン";
+        }
+        else if (turn_count % 2 == 1)
+        {
+            turn_count++;
+            turn_t.text = "先攻";
+
+            turn2_t.text = "";
+
+            turn2_t.text = turn_count / 2 + 1 + "ターン";
+        }
+
+        Change_Color();
+    }
+
+    void Change_Color()
+    {
+        GameObject[] tagobjs = GameObject.FindGameObjectsWithTag("Coma");
+
+        for(int i = 0;i < tagobjs.Length;i++)
+        {
+            Coma coma = tagobjs[i].GetComponent<Coma>();
+            
+            if(coma.PalyerNo - 1 == turn_count % 2)
+            {
+                tagobjs[i].GetComponent<Renderer>().material.color = Color.blue;
+            }
+            else
+            {
+                tagobjs[i].GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
+
+    }
 }
